2f243cae6f513be553dd918956d23dead33b668d
Mike Ward <mike@mike-ward.net>
2019-02-20 19:18:42 -0500

    suppress dumb npm warning messages

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 pushd src\App\js
-call npm i
+call npm install --loglevel=error --no-update-notifier
 popd
 pushd src
 msbuild CoreSample.sln /t:Restore;Rebuild /p:Configuration=Release /p:DeployOnBuild=true /verbosity:minimal
