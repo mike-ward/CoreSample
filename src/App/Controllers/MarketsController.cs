baecf7bdd0525bd51ad26de86550e83f341e46ca
Mike Ward <mike@mike-ward.net>
2019-02-28 21:17:14 -0500

    use ControllerBase

--- src/App/Controllers/MarketsController.cs
+++ src/App/Controllers/MarketsController.cs
@@ -1,33 +1,32 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Net;
-
-using Microsoft.AspNetCore.Mvc;
 
 namespace App.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]/[action]")]
-    public class MarketsController : Controller
+  [ApiController]
+  [Route("api/[controller]/[action]")]
+  public class MarketsController : ControllerBase
+  {
+    public ActionResult News() => Get("https://api.iextrading.com/1.0/stock/aapl/news");
+
+    [ActionName("most-active")]
+    public ActionResult MostActive() => Get("https://api.iextrading.com/1.0/stock/market/list/mostactive");
+
+    public ActionResult Gainers() => Get("https://api.iextrading.com/1.0/stock/market/list/gainers");
+
+    public ActionResult Losers() => Get("https://api.iextrading.com/1.0/stock/market/list/losers");
+
+    public ActionResult Symbols() => Get("https://api.iextrading.com/1.0/ref-data/symbols");
+
+    private static ContentResult Get(string url)
     {
-        public ActionResult News() => Get("https://api.iextrading.com/1.0/stock/aapl/news");
-
-        [ActionName("most-active")]
-        public ActionResult MostActive() => Get("https://api.iextrading.com/1.0/stock/market/list/mostactive");
-
-        public ActionResult Gainers() => Get("https://api.iextrading.com/1.0/stock/market/list/gainers");
-
-        public ActionResult Losers() => Get("https://api.iextrading.com/1.0/stock/market/list/losers");
-
-        public ActionResult Symbols() => Get("https://api.iextrading.com/1.0/ref-data/symbols");
-
-        private static ContentResult Get(string url)
-        {
-            using (var client = new WebClient())
-            {
-                var data = client.DownloadString(new Uri(url));
-                if (data is null) throw new InvalidProgramException("oops");
-                return new ContentResult { Content = data, ContentType = "application/json" };
-            }
-        }
+      using (var client = new WebClient())
+      {
+        var data = client.DownloadString(new Uri(url));
+        if (data is null) throw new InvalidProgramException("oops");
+        return new ContentResult { Content = data, ContentType = "application/json" };
+      }
     }
+  }
 }
