10dc0032f5eb029170be825a03b8e5ab9e8c0b1c
Mike Ward <mike@mike-ward.net>
2019-03-13 21:21:08 -0400

    perf natural string compare

--- src/App/js/services/compare-service.ts
+++ src/App/js/services/compare-service.ts
@@ -1,34 +1,15 @@
 ﻿export const compareService = {
-  compareAny: compareAny,
   naturalStringCompare: naturalStringCompare,
   naturalStringCompareIgnoreCase: naturalStringCompareIgnoreCase,
   locale: () => locale
 }
 
 const locale = 'en';
-
-function compareAny(a: any, b: any): number {
-  if (a === b) return 0;
-
-  // NaN, and only NaN, will compare unequal to itself,
-  // and is more reliable than isNaN(). Adding insult
-  // to injury, NaN itself is not a number.
-  const aIsNaN = a !== a;
-  const bIsNaN = b !== b;
-
-  if (typeof a === 'number' || typeof b === 'number' || aIsNaN || bIsNaN) {
-    if (aIsNaN && bIsNaN) return 0;
-    if (aIsNaN && !bIsNaN) return -1;
-    if (!aIsNaN && bIsNaN) return +1;
-    if (a === undefined && b !== undefined) return -1;
-    if (a !== undefined && b === undefined) return +1;
-    return a - b;
-  }
-
-  if (a !== null && b === null) return +1;
-  if (a === null && b !== null) return -1;
-  return a.localeCompare(b, locale);
-}
+const zero = 48;
+const nine = 57;
+const comma = 44;
+const decimal = 46;
+const IsNumber = (a: any) => !isNaN(a);
 
 function naturalStringCompare(a: string | number, b: string | number): number {
   return naturalStringCompareImplementation(a, b, undefined);
@@ -39,42 +20,43 @@ function naturalStringCompareIgnoreCase(a: string | number, b: string | number):
 }
 
 function naturalStringCompareImplementation(a: string | number, b: string | number, options: any): number {
-  enum ClassificationType { Undecided, Alpha, Number }
-  type ChunkType = { content: string, classification: ClassificationType, count: number };
-  const isNumberLike = (a: any) => !isNaN(a);
+  enum Classification { Alpha, Number }
+  type Chunk = { chars: string, classification: Classification, count: number };
 
-  function getChunk(str: string): ChunkType {
-    const len = str.length;
-    const chars: string[] = [];
-    let classification: ClassificationType = ClassificationType.Undecided;
+  function getChunk(str: string): Chunk {
     let count = 0;
+    let chars = '';
+    let classification;
+    let classified = false;
+    const len = str.length;
 
     // loop and index for performance
-    for (let idx = 0; idx < len; ++idx) {
-      count++;
-      const c = str.charAt(idx);
-      if (classification === ClassificationType.Undecided) {
-        classification = isDigit(c) ? ClassificationType.Number : ClassificationType.Alpha;
-      }
-      else if (classification === ClassificationType.Number && c === ',') {
-        continue;
+    while (count < len) {
+      const ch = str.charAt(count++);
+      const code = ch.charCodeAt(0);
+
+      if (classified) {
+        if (code === comma && classification === Classification.Number) continue;
+        const digitOrDecimal = code >= zero && code <= nine || code === decimal;
+        if (digitOrDecimal && classification === Classification.Alpha) break;
+        if (!digitOrDecimal && classification === Classification.Number) break;
       }
       else {
-        const numeric = isDigit(c);
-        if (classification === ClassificationType.Alpha && numeric) break;
-        if (classification === ClassificationType.Number && !numeric) break;
+        classified = true;
+        classification = code >= zero && code <= nine ? Classification.Number : Classification.Alpha;
       }
-      chars[chars.length] = c; // this is faster in most browsers compared to push
+
+      chars += ch;
     }
 
-    return { content: chars.join(''), classification: classification, count: count };
+    return { chars: chars, classification: classification, count: count };
   }
 
   while (true) {
     if (a === null && b === null) return 0;
     if (a === null) return -1;
     if (b === null) return 1;
-    if (isNumberLike(a) && isNumberLike(b)) return +a - +b;
+    if (IsNumber(a) && IsNumber(b)) return +a - +b;
 
     a = a.toString();
     b = b.toString();
@@ -83,25 +65,20 @@ function naturalStringCompareImplementation(a: string | number, b: string | numb
     const ac = getChunk(a);
     const bc = getChunk(b);
 
-    if (ac.classification === ClassificationType.Number
-      && bc.classification === ClassificationType.Number
-      && isNumberLike(ac.content) && isNumberLike(bc.content)) {
-      const compare = +ac.content - +bc.content;
+    if (ac.classification === Classification.Number
+      && bc.classification === Classification.Number
+      && IsNumber(ac.chars)
+      && IsNumber(bc.chars)) {
+      const compare = +ac.chars - +bc.chars;
       if (compare < 0) return -1;
       if (compare > 0) return 1;
     }
     else {
-      const result = ac.content.localeCompare(bc.content, locale, options);
+      const result = ac.chars.localeCompare(bc.chars, locale, options);
       if (result !== 0) return result;
     }
 
     a = a.substring(ac.count);
     b = b.substring(bc.count);
-  }
-
-  function isDigit(ch: string) {
-    // see https://jsperf.com/isdigit-test
-    const code = ch.charCodeAt(0);
-    return code > 47 && code < 58 || code === 46 // period
   }
 }
